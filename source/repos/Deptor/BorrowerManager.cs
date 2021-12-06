954cd344060740b9914e1d9a5456ed624cb406be
tomekkzielinski <tomasz.zielinski98tz@gmail.com>
2021-12-06 23:07:56 +0100

    add new proj

---
+++ source/repos/Deptor/BorrowerManager.cs
@@ -0,0 +1,20 @@
+﻿namespace Debtor
+{
+    internal class BorrowerManager
+    {
+        internal void AddBorrower(string? userName, decimal amountInDecimal)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void DeleteBorrower(string? userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void ListBorrowers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ source/repos/Deptor/DebtorApp.cs
@@ -0,0 +1,68 @@
+﻿namespace Debtor
+{
+    public class DebtorApp
+    {
+         BorrowerManager BorrowerManager { get; set; }    = new BorrowerManager();
+        public void IntroduceDebtorApp()
+        {
+
+        }
+        public void AddBorrower()
+        {
+            Console.WriteLine("Podaj nazwę dłużnika, którego chcesz dodać do listy");
+
+            var userName = Console.ReadLine();
+
+            Console.WriteLine("Podaj kwotę długu");
+
+          var userAmount = Console.ReadLine();  
+
+            var amountInDecimal = default(decimal);
+
+            while (!decimal.TryParse(userAmount, out amountInDecimal))
+            {
+                Console.WriteLine("podano niepoprawną kwotę");
+
+                Console.WriteLine("Podaj kwotę długu");
+
+                 userAmount = Console.ReadLine();
+
+            }
+
+            BorrowerManager.AddBorrower(userName, amountInDecimal);
+
+
+        }
+
+        public void DeleteBorrower()
+        {
+            Console.WriteLine("Podaj nazwę dłużnika, którego chcesz usunąć");
+
+            var userName = Console.ReadLine();
+
+            BorrowerManager.DeleteBorrower(userName);
+
+            Console.WriteLine("udało się usunać dłużnika");
+       
+        }
+
+        public void ListAllBorrowers()
+        {
+            Console.WriteLine("Oto lista Twoich dłużników:");
+
+
+            foreach (var borrower in BorrowerManager.ListBorrowers())
+            {
+               
+                Console.WriteLine(borrower); 
+            }
+
+          
+        }
+
+        public void AskForAction()
+        {
+
+        }
+    }
+}
